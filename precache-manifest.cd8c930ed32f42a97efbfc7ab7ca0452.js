a50f927f4729546217429952ec62b848b294e7ad
Pamela Cinthya Torres Gutiérrez <pamelatgtz@gmail.com>
2019-12-10 18:51:15 -0600

    Updates

---
+++ precache-manifest.cd8c930ed32f42a97efbfc7ab7ca0452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ac5d5861cc77445a3d34e37092df02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ac243d667da4e9e3e4f",
+    "url": "./static/css/2.71746c6c.chunk.css"
+  },
+  {
+    "revision": "d0cf879e78effaa998a7",
+    "url": "./static/css/main.d98ba3f4.chunk.css"
+  },
+  {
+    "revision": "5ac243d667da4e9e3e4f",
+    "url": "./static/js/2.d8a18828.chunk.js"
+  },
+  {
+    "revision": "d0cf879e78effaa998a7",
+    "url": "./static/js/main.ef03a9d5.chunk.js"
+  },
+  {
+    "revision": "33f58d7dbeba69ff4d31",
+    "url": "./static/js/runtime-main.a4a91e9b.js"
+  }
+]);
